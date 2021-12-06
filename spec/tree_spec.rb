4f6f9415444c6af3be304a6c84a067d76060e007
Michael Brandt <msbrandt00@gmail.com>
2021-12-06 10:37:08 -0700

    Add testing for #insert for parent node

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -1,6 +1,6 @@
 require './lib/node'
 require './lib/tree'
-
+require 'pry'
 RSpec.describe BinarySearchTree do
   before(:each) do
     @tree = BinarySearchTree.new()
@@ -11,5 +11,15 @@ RSpec.describe BinarySearchTree do
     it 'first node will be root' do
       expect(@tree.root.value).to eq(40)
     end
+  end
+  describe 'Insert' do
+   it 'will return the depth of new node after insertion' do
+     expect(@tree.insert(30, 'Space Jam')).to eq(1)
+     expect(@tree.insert(90, 'Shawshank Redemption')).to eq(2)
+   end
+  end
+  it 'will set the previous node as the parent node' do
+    @tree.insert(30, 'Space Jam')
+    expect(@tree.all_nodes.last.parent.value).to eq(40)
   end
 end
