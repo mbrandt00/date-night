31bbe1e15727b17027e505f8a182b22828f54181
Michael Brandt <msbrandt00@gmail.com>
2021-12-06 11:06:28 -0700

    Add #include? testing

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -17,7 +17,6 @@ RSpec.describe BinarySearchTree do
      expect(@tree.insert(30, 'Space Jam')).to eq(1)
      expect(@tree.insert(90, 'Shawshank Redemption')).to eq(2)
    end
-  end
   it 'will set the previous node as the parent node' do
     @tree.insert(30, 'Space Jam')
     expect(@tree.all_nodes.last.parent.value).to eq(40)
@@ -25,9 +24,19 @@ RSpec.describe BinarySearchTree do
   it 'will add the right and left node information to new nodes' do
     @tree.insert(50, 'Mission Impossible')
     @tree.insert(90, '12 angry men')
-    expect(@tree.all_nodes[-3].right.name).to eq('12 angry men')
+    expect(@tree.all_nodes[-3].right.movie).to eq('12 angry men')
     # right of 'A beautiful mind is '12 angry men'
     expect(@tree.all_nodes[-3].left.value).to eq(50)
     # left of 'A beautiful mind is Mission Impossible'
+    end
+  end
+
+  describe 'Include' do
+    it 'will return true if value is included in tree' do
+      expect(@tree.include?(40)).to be(true)
+    end
+    it 'will return false if value is not included in tree' do
+      expect(@tree.include?(10)).to be(false)
+    end
   end
 end
